6a85bc1fecceccce637765eb7a9bed676d29b97c
mrroot5 <mrroot5@protonmail.com>
2025-06-30 13:22:15 +0200

    Initial commit

---
+++ lib/bank_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule BankWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use BankWeb, :controller` and
+  `use BankWeb, :live_view`.
+  """
+  use BankWeb, :html
+
+  embed_templates "layouts/*"
+end
