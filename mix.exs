f0d827a2aa5db2976df77be56bcd4627df2e2397
mrroot5 <mrroot5@protonmail.com>
2025-07-09 17:27:26 +0200

    Added recode library to automatically fix some credo errors

--- mix.exs
+++ mix.exs
@@ -6,8 +6,8 @@ defmodule Bank.MixProject do
       app: :bank,
       version: "0.1.0",
       elixir: "~> 1.14",
-      elixirc_paths: elixirc_paths(Mix.env()),
-      start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(get_mix_env()),
+      start_permanent: get_mix_env() == :prod,
       aliases: aliases(),
       deps: deps()
     ]
@@ -34,12 +34,12 @@ defmodule Bank.MixProject do
     [
       {:argon2_elixir, "~> 3.0"},
       {:bandit, "~> 1.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: :dev, runtime: false},
       {:decimal, "~> 2.0"},
       {:dns_cluster, "~> 0.2.0"},
       {:ecto_sql, "~> 3.10"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:excellent_migrations, "~> 0.1", only: [:dev, :test], runtime: false},
+      {:esbuild, "~> 0.8", runtime: get_mix_env() == :dev},
+      {:excellent_migrations, "~> 0.1", only: :dev, runtime: false},
       {:ex_money, "~> 5.0"},
       {:finch, "~> 0.13"},
       {:floki, ">= 0.30.0", only: :test},
@@ -59,7 +59,8 @@ defmodule Bank.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.0"},
       {:postgrex, ">= 0.0.0"},
-      {:tailwind, "~> 0.3.1", runtime: Mix.env() == :dev},
+      {:recode, "~> 0.7", only: :dev, runtime: false},
+      {:tailwind, "~> 0.3.1", runtime: get_mix_env() == :dev},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:swoosh, "~> 1.5"}
@@ -87,4 +88,13 @@ defmodule Bank.MixProject do
       ]
     ]
   end
+
+  # Get mix env from system environment or mix library.
+  @spec get_mix_env :: atom()
+  defp get_mix_env, do: get_mix_env(System.get_env("MIX_ENV"))
+
+  # TODO Esto deberia fallar y pedir que se agregue el segundo tipo de spec ya que este tiene un arity de 1 no de 0
+  defp get_mix_env(nil), do: Mix.env()
+
+  defp get_mix_env(mix_env) when is_binary(mix_env), do: String.to_atom(mix_env)
 end
