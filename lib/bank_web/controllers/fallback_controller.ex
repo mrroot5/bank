a83769e7df2bfb70eb367b6031a9e3f8d89d8c33
mrroot5 <mrroot5@protonmail.com>
2025-07-24 09:37:12 +0200

    Added bodyguard policies and block admin panel access

---
+++ lib/bank_web/controllers/fallback_controller.ex
@@ -0,0 +1,16 @@
+defmodule BankWeb.FallbackController do
+  @moduledoc """
+  Handle responses not matched by any other controller.
+
+  This is a Phoenix plug called `action_fallback`, check
+  [the docs for more info](https://hexdocs.pm/phoenix/Phoenix.Controller.html#action_fallback/1).
+  """
+  use BankWeb, :controller
+
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(:forbidden)
+    |> put_view(html: BankWeb.ErrorHTML)
+    |> render(:"403")
+  end
+end
---
+++ lib/bank_web/live/admin/policies.ex
@@ -0,0 +1,14 @@
+defmodule BankWeb.Admin.Policies do
+  @moduledoc """
+  General purpose authorization module for admin [policies](https://hexdocs.pm/bodyguard/Bodyguard.Policy.html)
+  """
+  @behaviour Bodyguard.Policy
+
+  use BankWeb, :controller
+
+  action_fallback BankWeb.FallbackController
+
+  def authorize(_, %{roles: roles}, _params), do: if(:superuser in roles, do: true, else: false)
+
+  def authorize(_action, _user, _params), do: false
+end
