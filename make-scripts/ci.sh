5996e030786dd1fe686aab4cb9a9861d5234481b
mrroot5 <mrroot5@protonmail.com>
2025-07-25 20:57:50 +0200

    Fix format

--- make-scripts/ci.sh
+++ make-scripts/ci.sh
@@ -1,9 +1,11 @@
-echo "Starting credo...\n"
+echo "Starting format...\n"
+docker compose exec web mix format
+echo "\nStarting credo...\n"
 docker compose exec web mix credo --strict
-echo "Starting recode suggestions....\n"
+echo "\nStarting recode suggestions....\n"
 docker compose exec web mix recode
-echo "\nApply recode suggestions with make recode\n"
-echo "Starting dialyzer plt...\n"
+echo "\nAPPLY RECODE SUGGESTIONS WITH: make recode\n"
+echo "\nStarting dialyzer plt...\n"
 docker compose exec web mix dialyzer --plt
 echo "Starting dialyzer...\n"
 docker compose exec web mix dialyzer --quiet-with-result
