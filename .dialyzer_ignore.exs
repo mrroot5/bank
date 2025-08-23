59a4bdb057b8faafa9d9400054b3ca8cb125ac93
mrroot5 <mrroot5@protonmail.com>
2025-08-23 16:13:59 +0200

    Fix dialyzer ignore

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -3,7 +3,7 @@
   ~r/lib\/bank\/users_settings.ex:129/,
   ~r/lib\/bank\/users_settings.ex:107/,
   ~r/lib\/bank\/users_sessions.ex:91/,
-  ~r/lib\/bank\/users.ex:195/,
+  ~r/lib\/bank\/users.ex:207/,
   ~r/lib\/bank_web\/components\/core_components.ex:615/,
   ~r/lib\/bank_web\/components\/core_components.ex:627/,
   ~r/lib\/bank_web\/components\/core_components.ex:639/,
