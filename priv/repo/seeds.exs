b0ee183a1412430f48c54d66ed9309045353bb4c
mrroot5 <mrroot5@protonmail.com>
2025-09-07 17:06:39 +0200

    Call seeds modules from repo seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# Seeding
+# To force seeding even if data exist, pass force: true
+
+default_count = "COUNT_SEED" |> System.get_env("10") |> String.to_integer()
+
+force? = "FORCE_SEED" |> System.get_env("true") |> String.to_atom()
+
+Bank.Seeds.Users.seed(default_count, force: force?)
+Bank.Seeds.Accounts.seed(default_count, force: force?)
+Bank.Seeds.Transactions.seed(default_count, force: force?)
+Bank.Seeds.Ledgers.seed(force: force?)
